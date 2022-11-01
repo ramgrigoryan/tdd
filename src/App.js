cd54eb88f383982b17f2fa7cc0327ec0a659dfe7
Vahram Grigoryan <vahram.grigoryan@bostongene.com>
2022-11-01 21:43:29 +0400

    First steps in tdd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [colorState,setColorState] = useState(true)
+  return <div className='App'>
+    <button style={{backgroundColor:colorState? "red":"green"}} onClick={()=>setColorState(!colorState)}>{colorState?"Turn to green":"Turn to red"}</button>
+  </div>
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('red button with initial "Turn to green"',()=>{
+  render(<App/>)
+  
+  // * find an element with the role of button and with text "Turn to green" 
+
+  const colorButton = screen.getByRole('button',{name:/Turn to green/i})
+  
+  //* expect colorButton to have a red background color
+
+  expect(colorButton).toHaveStyle({backgroundColor:"red"})
+
+  fireEvent.click(colorButton)
+
+  //* expect colorButton to have green bg color and inner text with "Turn to red"
+
+  expect(colorButton).toHaveStyle({backgroundColor:"green"});
+
+  expect(colorButton).toHaveTextContent('Turn to red');
+})
