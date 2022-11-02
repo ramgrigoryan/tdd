1c51d4b9b253a5d15ab4a000bfaebd9c8e8fb208
Vahram Grigoryan <vahram.grigoryan@bostongene.com>
2022-11-02 13:37:27 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 
+
+export const spaceInseadOfCamelCase = (colorName)=> colorName.replace(/(\B[A-Z])/g, " $1")
+
 const App = () => {
   const [colorState,setColorState] = useState(true)
   const [buttonState,setButtonState] = useState(false);
   return <div className='App'>
-    <button style={{backgroundColor:colorState? "red":"green"}} onClick={()=>setColorState(!colorState)} disabled={buttonState}>{colorState?"Turn to green":"Turn to red"}</button>
-    <input type='checkbox' onClick={()=>{setButtonState(!buttonState)}} />
+    <button style={{backgroundColor:buttonState?"grey":colorState? "red":"green"}} onClick={()=>setColorState(!colorState)} disabled={buttonState}>{colorState?"Turn to green":"Turn to red"}</button>
+    <label htmlFor='disableCheckbox'>disable colored button</label>
+    <input id="disableCheckbox" type='checkbox' onClick={()=>{setButtonState(!buttonState)}} />
   </div>
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,32 +1,49 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App, { spaceInseadOfCamelCase } from "./App";
 
-test('red button with initial "Turn to green"',()=>{
-  render(<App/>)
-  
-  // * find an element with the role of button and with text "Turn to green" 
+test('red button with initial "Turn to green"', () => {
+  render(<App />);
 
-  const colorButton = screen.getByRole('button',{name:/Turn to green/i})
-  
-  //* expect colorButton to have a red background color
+  // * find an element with the role of button and with text "Turn to green"
 
-  expect(colorButton).toHaveStyle({backgroundColor:"red"})
+  const colorButton = screen.getByRole("button", { name: /Turn to green/i });
 
-  fireEvent.click(colorButton)
+  //* expect colorButton to have initial red background color
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click(colorButton);
 
   //* expect colorButton to have green bg color and inner text with "Turn to red"
 
-  expect(colorButton).toHaveStyle({backgroundColor:"green"});
+  expect(colorButton).toHaveStyle({ backgroundColor: "green" });
 
-  expect(colorButton).toHaveTextContent('Turn to red');
-})
+  expect(colorButton).toHaveTextContent("Turn to red");
+});
 
-test('find checkbox and test is it making button to be disable',()=>{
-  render(<App/>)
-  const checkbox = screen.getByRole('checkbox')
-  const colorButton = screen.getByRole('button',{name:/Turn to/i})
+test("find checkbox and test is it making button to be disable", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: "disable colored button",
+  });
+  const colorButton = screen.getByRole("button", { name: /Turn to/i });
 
   fireEvent.click(checkbox);
 
   expect(colorButton).toBeDisabled();
-})
+  expect(colorButton).toHaveStyle({ backgroundColor: "grey" });
+});
+
+describe("function return string where camel case is preceded with space", () => {
+  test("f-n return right value for one color shade", () => {
+    expect(spaceInseadOfCamelCase("Red")).toBe("Red");
+  });
+  test("f-n return right value for 2 color shades", () => {
+    expect(spaceInseadOfCamelCase("MidnightBlue")).toBe("Midnight Blue");
+  });
+  test("f-n return right value for color with any shades", () => {
+    expect(spaceInseadOfCamelCase("VeryDarkRedMixedWithSunriseCoral")).toBe(
+      "Very Dark Red Mixed With Sunrise Coral"
+    );
+  });
+});
